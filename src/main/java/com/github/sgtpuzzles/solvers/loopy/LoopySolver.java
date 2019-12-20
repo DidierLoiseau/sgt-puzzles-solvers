5493fd434a396d4be05768761a50573993ac9869
DidierLoiseau <didierloiseau+github@gmail.com>
2019-12-20 14:14:05 +0100

    Provide a Logger to rules and log Drools execution if in trace

--- src/main/java/com/github/sgtpuzzles/solvers/loopy/LoopySolver.java
+++ src/main/java/com/github/sgtpuzzles/solvers/loopy/LoopySolver.java
@@ -3,8 +3,10 @@ package com.github.sgtpuzzles.solvers.loopy;
 import com.github.sgtpuzzles.grid.model.Graph;
 import com.github.sgtpuzzles.solvers.loopy.drools.CountOrNone;
 import com.github.sgtpuzzles.solvers.loopy.drools.Exactly;
+import lombok.extern.slf4j.Slf4j;
 import org.drools.core.command.runtime.rule.QueryCommand;
 import org.kie.api.KieServices;
+import org.kie.api.logger.KieRuntimeLogger;
 import org.kie.api.runtime.rule.QueryResults;
 
 import java.util.HashSet;
@@ -13,7 +15,9 @@ import java.util.Map;
 import java.util.stream.StreamSupport;
 
 import static java.util.stream.Collectors.toList;
+import static org.kie.internal.logger.KnowledgeRuntimeLoggerFactory.newFileLogger;
 
+@Slf4j
 public class LoopySolver {
 
 	public List<Move> solve(Graph graph, Map<Integer, Integer> clues) {
@@ -21,23 +25,30 @@ public class LoopySolver {
 		var kContainer = kieServices.getKieClasspathContainer();
 
 		var kieSession = kContainer.newKieSession();
+		kieSession.setGlobal("log", log);
 
 		graph.getEdges().forEach(kieSession::insert);
 
 		graph.getVertices().values().stream()
 				.map(v -> new CountOrNone(new HashSet<>(v.getEdges().values()), 2))
 				.forEach(kieSession::insert);
-//		var edgeStates = graph.getEdges().stream()
-//				.map(EdgeState::new)
-//				.collect(toMap(EdgeState::getEdge, es -> es));
-//		edgeStates.values().forEach(kieSession::insert);
 
 		graph.getFaces().stream()
 				.filter(f -> clues.containsKey(f.getId()))
 				.map(f -> new Exactly(f.getEdges(), clues.get(f.getId())))
 				.forEach(kieSession::insert);
 
-		kieSession.fireAllRules();
+		KieRuntimeLogger logger = log.isTraceEnabled()
+				? newFileLogger(kieSession, "drools")
+				: () -> {
+		};
+		try {
+			kieSession.fireAllRules();
+		} catch (Exception e) {
+			log.error("Caught exception while processing rules", e);
+		} finally {
+			logger.close();
+		}
 
 		QueryResults result = kieSession.execute(new QueryCommand("move", "getMoves"));
 		var moves = StreamSupport.stream(result.spliterator(), false)
